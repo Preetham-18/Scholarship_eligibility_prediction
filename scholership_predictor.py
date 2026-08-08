be5cc2696b02e15f470a1d2fb380dbbed4b8f307
Preetham K P <pp9687365@gmail.com>
2026-08-08 11:44:42 +0530

    Implement sequential covering rule selection

--- scholership_predictor.py
+++ scholership_predictor.py
@@ -28,6 +28,37 @@ def sequential_covering(df):
 
         rules = []
 
+        rule = " If CGPA >= " + str(min_cgpa) + " THEN Scholarship = Eligible"
+        rules.append(rule)
+
+        wrongly_covered = not_eligible_students[not_eligible_students["CGPA"] >= min_cgpa]
+
+        print("\n Wrongly covered students:")
+        print(wrongly_covered)
+
+        print("\n Number of students wrongly covered:", len(wrongly_covered))
+
+        rule2 = " If CGPA >= " + str(min_cgpa) + " AND Backlogs = 0 " " THEN Scholarship = Eligible"
+        
+
+        wrongly_covered2 = not_eligible_students[
+                (not_eligible_students["CGPA"] >= min_cgpa) &
+                (not_eligible_students["Backlogs"] == 0)]
+
+        print("\n Wrongly covered by rule2:")
+        print(wrongly_covered2)
+
+        print("\n Wrongly covered students by rule2:", len(wrongly_covered2))
+
+
+        if len(wrongly_covered2) < len(wrongly_covered):
+                rules[0] = rule2
+        else:
+                rules[0] = rule
+
+        print("\n Selected Rule:")
+        print(rules[0])
+                  
 
 print("\nSequential Covering Algorithm:")
 sequential_covering(df)
